3269f426fc4e8480a4dc533a3733051a923e5fb0
Ian Mercer <ian@signswift.com>
2020-05-08 16:13:18 +0100

    Initial version hacked from BlueZ and MQTT sources Removed all dependencies to make self-contained project

---
+++ client/build.sh
@@ -0,0 +1,21 @@
+echo "Compiling bluetoothctl"
+
+gcc -I. \
+  -I/usr/include/dbus-1.0 \
+  -I/usr/lib/arm-linux-gnueabihf/dbus-1.0/include \
+  -I/usr/include/glib-2.0 \
+  -I/usr/include/bluetooth \
+  -I/usr/lib/arm-linux-gnueabihf/glib-2.0/include \
+  -g -O2 -MT main.o -MD -MP -MF sniffer.Tpo -c main.c advertising.c agent.c display.c gatt.c mqtt.c mqtt_pal.c
+
+echo "Linking bluetoothctl"
+
+gcc -g -O2 -o bluetoothctl main.o display.o agent.o advertising.o gatt.o mqtt.o mqtt_pal.o \
+  ../gdbus/.libs/libgdbus-internal.a ../src/.libs/libshared-glib.a -lglib-2.0 -lpthread -ldbus-1 -lreadline
+
+
+echo "Running"
+
+sudo ./bluetoothctl
+
+
