8520532b27609c57fb3d041bb57167939c6d7f8e
Ian Mercer <ian@signswift.com>
2020-06-26 05:06:10 +0000

    Move Kalman filter to separate file

---
+++ kalman.c
@@ -0,0 +1,46 @@
+#include <glib.h>
+#include <stdbool.h>
+#include <stdio.h>
+#include <time.h>
+#include <math.h>
+
+/*
+      Kalman filter
+*/
+
+struct Kalman
+{
+    double err_measure;
+    double err_estimate;
+    double q;
+    double current_estimate;
+    double last_estimate;
+    double kalman_gain;
+};
+
+void kalman_initialize(struct Kalman *k)
+{
+    k->current_estimate = -999; // marker value used by time interval check
+    k->last_estimate = -999; // marker value, so first real value overrides it
+    k->err_measure = 10.0;
+    k->err_estimate = 10.0;
+    k->q = 0.25;   // was 0.25 which was too slow
+}
+
+float kalman_update(struct Kalman *k, double mea)
+{
+    // First time through, use the measured value as the actual value
+    if (k->last_estimate == -999)
+    {
+        k->last_estimate = mea;
+        return mea;
+    }
+    //g_print("%f %f %f %f\n", k->err_measure, k->err_estimate, k->q, mea);
+    k->kalman_gain = k->err_estimate / (k->err_estimate + k->err_measure);
+    k->current_estimate = k->last_estimate + k->kalman_gain * (mea - k->last_estimate);
+    k->err_estimate = (1.0 - k->kalman_gain) * k->err_estimate + fabs(k->last_estimate - k->current_estimate) * k->q;
+    k->last_estimate = k->current_estimate;
+
+    return k->current_estimate;
+}
+
--- scan.c
+++ scan.c
@@ -30,6 +30,7 @@ char* client_id = NULL;
 
 #include "utility.c"
 #include "mqtt_send.c"
+#include "kalman.c"
 
 // delta distance x delta time threshold for sending an update
 // e.g. 5m after 2s or 1m after 10s
@@ -40,46 +41,6 @@ char* client_id = NULL;
 
 // Handle Ctrl-c
 void     int_handler(int);
-
-/*
-      Kalman filter
-*/
-
-struct Kalman
-{
-    double err_measure;
-    double err_estimate;
-    double q;
-    double current_estimate;
-    double last_estimate;
-    double kalman_gain;
-};
-
-void kalman_initialize(struct Kalman *k)
-{
-    k->current_estimate = -999; // marker value used by time interval check
-    k->last_estimate = -999; // marker value, so first real value overrides it
-    k->err_measure = 10.0;
-    k->err_estimate = 10.0;
-    k->q = 0.25;   // was 0.25 which was too slow
-}
-
-float kalman_update(struct Kalman *k, double mea)
-{
-    // First time through, use the measured value as the actual value
-    if (k->last_estimate == -999)
-    {
-        k->last_estimate = mea;
-        return mea;
-    }
-    //g_print("%f %f %f %f\n", k->err_measure, k->err_estimate, k->q, mea);
-    k->kalman_gain = k->err_estimate / (k->err_estimate + k->err_measure);
-    k->current_estimate = k->last_estimate + k->kalman_gain * (mea - k->last_estimate);
-    k->err_estimate = (1.0 - k->kalman_gain) * k->err_estimate + fabs(k->last_estimate - k->current_estimate) * k->q;
-    k->last_estimate = k->current_estimate;
-
-    return k->current_estimate;
-}
 
 /*
    Structure for reporting to MQTT
