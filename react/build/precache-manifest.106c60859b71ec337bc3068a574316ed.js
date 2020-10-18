5ff0b60e6d739b205b87b1927c2b8faececb6686
Ian Mercer <ian@signswift.com>
2020-10-18 11:03:01 -0700

    Include the built web site in git for now

---
+++ react/build/precache-manifest.106c60859b71ec337bc3068a574316ed.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "825df6e94ffa581a1d16d7cfd5b77ba4",
+    "url": "/index.html"
+  },
+  {
+    "revision": "22f4ccc6061c94244992",
+    "url": "/static/css/main.5ecd60fb.chunk.css"
+  },
+  {
+    "revision": "1f61ffcc0edd0ff5878f",
+    "url": "/static/js/2.b0c4f43a.chunk.js"
+  },
+  {
+    "revision": "b13e737c23346402089bf79c557ecf58",
+    "url": "/static/js/2.b0c4f43a.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "22f4ccc6061c94244992",
+    "url": "/static/js/main.3f51d037.chunk.js"
+  },
+  {
+    "revision": "39c95e27ca78c7c7403b",
+    "url": "/static/js/runtime-main.4f6b4dea.js"
+  }
+]);
